d466a8e12d7096fc2becd0d5f51213dc0a61817d
Michael Gruber <mail@michaelgruber.me>
2014-04-11 09:36:11 -0400

    added pgrep like function with a little more info

--- .bin/grep_kill.sh
+++ .bin/grep_kill.sh
@@ -1,19 +1,36 @@
-grep_kill () {
-    processes=$(ps -e | grep "$@" | grep -v grep)
+__processes=''
 
-    if [ "$processes" == '' ]; then
+__grep_process () {
+    __processes=$(ps -e | grep "$@" | grep -v grep)
+
+    if [ "$__processes" == '' ]; then
         echo "No processes matching $@."
+        return 1
     else
-        hit_list="$(echo "$processes" | awk '{print $1}' )"
-
-       echo "$hit_list" | xargs kill -9
-        __announce_slain "$processes"
+        return 0
     fi
 }
 
-__announce_slain () {
-    slain="$(echo "$1" | awk '{ if ($5) { print $5 } else { print $4 } }' )"
+__announce () {
+    slain="$(echo "$1" | awk '{ if ($5) { print $1, $5 } else { print $1, $4 } }' )"
 
-    echo "KILLED:"
-    echo "$(echo "$slain" | awk -F/ '{print $NF}' )"
+    echo "$(echo "$slain" | awk -F/ '{ print $1, $NF }' )"
+}
+
+grep_kill () {
+    if __grep_process $@; then
+       hit_list="$(echo "$__processes" | awk '{print $1}' )"
+
+       echo "$hit_list" | xargs kill -9
+
+       echo "KILLED"
+        __announce "$__processes"
+    fi
+}
+
+grep_process_list () {
+    if __grep_process $@; then
+        echo "PROCESSES:"
+        __announce "$__processes"
+    fi
 }
