2bea1864aa94042781e7659b4c85b52047938dc6
Michael Gruber <mail@michaelgruber.me>
2014-07-29 16:50:50 -0400

    git start if theres already a gitignore

--- .bin/git_start.sh
+++ .bin/git_start.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 git init
-touch .gitignore
-git add .
+
+if [ -e .gitignore ]
+then
+    mv .gitignore ._gitignore
+    touch .gitignore
+    git add .gitignore
+    mv ._gitignore .gitignore
+else
+    touch .gitignore
+    git add .gitignore
+fi
+
 git commit -m 'initial commit'
 git checkout -b develop
